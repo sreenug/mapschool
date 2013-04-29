4dc376cbf63b96950eb409ea6670042732db1800
Sreenu <sreenivasula@digitalgreen.org>
2013-04-29 17:11:55 +0530

    --get request values added

--- mapschool/views.py
+++ mapschool/views.py
@@ -3,8 +3,10 @@ from django.http import Http404, HttpResponse, HttpResponseNotFound, QueryDict
 from django.utils import simplejson
 
 def add_school(request):
-	name = request.GET.get('name', None)
-	return HttpResponse(name)
+	name = request.GET.get('name', "n")
+	test = request.GET.get('test', "n")
+	rt = name + test
+	return HttpResponse(rt)
 
 def tastypie_post(request):
     return render_to_response('tastypie_post.html')
