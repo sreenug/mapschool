9cdfc297ff68a682aed00a8878cb90953f481761
Sreenu <sreenivasula@digitalgreen.org>
2013-04-29 15:20:42 +0530

    modified view

--- mapschool/views.py
+++ mapschool/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect, render, render_to_response
 from django.http import Http404, HttpResponse, HttpResponseNotFound, QueryDict
+import simplejson
 
 def add_school(request):
-    return HttpResponse('success')
+    return HttpResponse(simplejson.dumps({"id":"10", "name":"sri"}), mimetype="application/json")
 
 def tastypie_post(request):
     return render_to_response('tastypie_post.html')
