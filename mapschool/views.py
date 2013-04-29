998a034084cf37a15a4e40a2e4b62c088ad53775
Sreenu <sreenivasula@digitalgreen.org>
2013-04-29 15:01:28 +0530

    --added sample view for testing ajax get request..

--- mapschool/views.py
+++ mapschool/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render, render_to_response
+from django.http import Http404, HttpResponse, HttpResponseNotFound, QueryDict
+
 def add_school(request):
-    return render_to_response('add_school.html')
+    return HttpResponse('success')
 
 def tastypie_post(request):
     return render_to_response('tastypie_post.html')
