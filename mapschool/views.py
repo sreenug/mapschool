e7ac90ed5968e821e21d6ff33abf49e40b5cbede
Sreenu <sreenivasula@digitalgreen.org>
2013-04-29 15:24:16 +0530

    --simple json import error

--- mapschool/views.py
+++ mapschool/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render, render_to_response
 from django.http import Http404, HttpResponse, HttpResponseNotFound, QueryDict
-import simplejson
+from django.utils import simplejson
 
 def add_school(request):
     return HttpResponse(simplejson.dumps({"id":"10", "name":"sri"}), mimetype="application/json")
